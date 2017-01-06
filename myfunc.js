07779a60bafd0d4a2234294f2c64e9e61307a3b2
Jdawleer <paul.riot@hotmail.fr>
2017-01-06 15:19:28 +0100

    Add files via upload

---
+++ myfunc.js
@@ -0,0 +1,71 @@
+function updated(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("Im1").src= "Images/1D/" + val + "1.png"
+    document.getElementById("Im2").src= "Images/1D/" + val + "2.png"
+    document.getElementById("Im3").src= "Images/1D/" + val + "3.png"
+ }
+ 
+ function updated2(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("Im4").src= "Images/1D/" + val + "1.png"
+    document.getElementById("Im5").src= "Images/1D/" + val + "2.png"
+    document.getElementById("Im6").src= "Images/1D/" + val + "3.png"
+ }
+
+ function updated3(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDeb1").src= "Images/2D/" + val + ".png"
+ }
+
+ function updated4(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDeb2").src= "Images/2D/" + val + ".png"
+ }
+ function updated5(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDebtext1").src= "Images/2D/" + val + ".png"
+ }
+ function updated6(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDebtext2").src= "Images/2D/" + val + ".png"
+ }
+ function updated7(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDebtext3").src= "Images/2D/" + val + ".png"
+ }
+ function updated8(element)
+{
+    var idx=element.selectedIndex;
+    var val=element.options[idx].value;
+    var content=element.options[idx].innerHTML;
+
+    document.getElementById("ImDebtext4").src= "Images/2D/" + val + ".png"
+ }
